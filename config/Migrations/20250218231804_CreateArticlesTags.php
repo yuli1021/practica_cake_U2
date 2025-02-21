41e7d77e7c2c70c1cbc69aac32c31c0e81be93da
yuli1021 <brenda.03pro@gmail.com>
2025-02-20 22:35:45 -0600

    first commit

---
+++ config/Migrations/20250218231804_CreateArticlesTags.php
@@ -0,0 +1,34 @@
+<?php
+declare(strict_types=1);
+
+use Migrations\AbstractMigration;
+
+class CreateArticlesTags extends AbstractMigration
+{
+    public $autoId = false;
+
+    /**
+     * Change Method.
+     *
+     * More information on this method is available here:
+     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
+     * @return void
+     */
+    public function change(): void
+    {
+        $table = $this->table('articles_tags');
+        $table->addColumn('created', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addColumn('modified', 'datetime', [
+            'default' => null,
+            'null' => false,
+        ]);
+        $table->addPrimaryKey([
+            'article_id',
+            'tag_id',
+        ]);
+        $table->create();
+    }
+}
